e0c8a6f23c225ac95570d9cfa9c95a3de1b0e46f
DzhoyNik <DzhoyNik@yandex.ru>
2025-09-24 21:49:46 +0700

    Init project

---
+++ Dichotomy.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator
+{
+    public partial class Dichotomy : Page
+    {
+        private string fun { get; set; }
+        private double rangeA { get; set; }
+        private double rangeB { get; set; }
+        private double accuracy { get; set; }
+
+        public Dichotomy()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleBackMenu(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Menu());
+        }
+
+        private void TextBox_F(object sender, TextChangedEventArgs e)
+        {
+            fun = ((TextBox)sender).Text;
+        }
+
+        private void TextBox_NumberChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = (TextBox)sender;
+            string input = textBox.Text.Replace('.', ',');
+            if (!double.TryParse(input, NumberStyles.Any, CultureInfo.InvariantCulture, out double value))
+            {
+                value = 0.0;
+                Console.WriteLine("Недопустимый формат данных!");
+            }
+
+            switch (textBox.Tag?.ToString())
+            {
+                case "A":
+                    rangeA = value;
+                    break;
+
+                case "B":
+                    rangeB = value;
+                    break;
+
+                case "E":
+                    accuracy = value;
+                    break;
+            }
+        }
+
+        private void HandleCalculate(object sender, RoutedEventArgs e)
+        {
+            DichtomyMethod dichtomyMethod = new DichtomyMethod(fun, rangeA, rangeB, accuracy);
+
+            if (!string.IsNullOrEmpty(fun) && accuracy > 0) 
+            {
+                double root = dichtomyMethod.Solve();
+
+                if (root != 0 || (rangeA == 0 || rangeB == 0))
+                {
+                    ChartBuilder chart = new ChartBuilder(fun);
+                    chart.Draw(ChartLine, rangeA, rangeB);
+                    chart.DrawRootPoint(ChartLine, root);
+                    inputRoot.Text = root.ToString();
+                }
+                else
+                {
+                    Console.WriteLine("Корень не найден");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Введите данные!");
+            }
+        }
+    }
+}
---
+++ controller/DichtomyMethod.cs
@@ -0,0 +1,103 @@
+﻿using Mathos.Parser;
+using System;
+
+class DichtomyMethod
+{
+    private readonly MathParser parser;
+    private string fun;
+    private double rangeA;
+    private double rangeB;
+    private double accuracy;
+    private double funA;
+    private double funB;
+    private double middleSegment;
+
+    public DichtomyMethod(string fun, double rangeA, double rangeB, double accuracy)
+    {
+        parser = new MathParser();
+        this.fun = fun;
+        this.rangeA = rangeA;
+        this.rangeB = rangeB;
+        this.accuracy = accuracy;
+        middleSegment = 0;
+        funA = 0;
+        funB = 0;
+    }
+
+    public bool CheckFunction()
+    {
+        try
+        {
+            parser.LocalVariables["x"] = 0;
+            parser.Parse(fun);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public bool CheckInterval()
+    {
+        parser.LocalVariables["x"] = rangeA;
+        funA = parser.Parse(fun);
+
+        parser.LocalVariables["x"] = rangeB;
+        funB = parser.Parse(fun);
+
+        if (Math.Abs(funA) < accuracy || Math.Abs(funB) < accuracy) return true;
+
+        return funA * funB < 0;
+    }
+
+    public double MiddleSegment()
+    {
+        return middleSegment = (rangeA + rangeB) / 2;
+    }
+
+    public double Solve()
+    {
+        if (!CheckFunction()) return 0;
+
+        parser.LocalVariables["x"] = rangeA;
+        funA = parser.Parse(fun);
+        if (Math.Abs(funA) < accuracy) return rangeA;
+
+        parser.LocalVariables["x"] = rangeB;
+        funB = parser.Parse(fun);
+        if (Math.Abs(funB) < accuracy) return rangeB;
+
+        if (!CheckInterval()) return 0;
+
+        int iteration = 0;
+
+        while ((rangeB - rangeA) / 2 > accuracy && iteration < 1000)
+        {
+            middleSegment = MiddleSegment();
+
+            parser.LocalVariables["x"] = middleSegment;
+            double funC = parser.Parse(fun);
+
+            if (Math.Abs(funC) < accuracy)
+            {
+                return middleSegment;
+            }
+
+            if (funA * funC < 0)
+            {
+                rangeB = middleSegment;
+                funB = funC;
+            }
+            else
+            {
+                rangeA = middleSegment;
+                funA = funC;
+            }
+
+            iteration++;
+        }
+
+        return middleSegment;
+    }
+}
